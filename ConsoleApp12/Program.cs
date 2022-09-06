5ceff162e5ccdeb92e660252ba860fb9a2d8e2c4
pc <pc@DESKTOP-P40ILKJ>
2022-09-06 10:08:43 +0100

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    // base class
+    class Brand
+    {
+
+        public string name;
+
+        public void display()
+        {
+            Console.WriteLine("I am a brand");
+        }
+
+    }
+
+    // derived class of Brand 
+    class YSL : Brand
+    {
+
+        public void getName()
+        {
+            Console.WriteLine("My brand name is " + name);
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // object of derived class
+            YSL labrador = new YSL();
+
+            // access field and method of base class
+            labrador.name = "LV";
+            labrador.display();
+
+            // access method from own class
+            labrador.getName();
+
+            Console.ReadLine();
+        }
+
+    }
+}
